760131ae62c8188d193853b651d6d7683e3320dd
No0ne155 <magnus@sunrise.ch>
2022-09-05 08:48:57 +0200

    Primzahlen korrigiert

--- exercises/04_Primzahlen.py
+++ exercises/04_Primzahlen.py
@@ -1,11 +1,8 @@
 primzahlen = [1,2,3,5,7]
-x = 2
-
+x = 1
 for i in range(2, 100):
     if i % 2 == 0 or i %3 == 0 or i % 5 == 0 or i % 7 == 0:
         x = x +1
     else:
         primzahlen.append(i)
-
-
 print(primzahlen)
