1a313a22c6fa8f29f60bdc0e78c792209dd101a1
No0ne155 <magnus@sunrise.ch>
2022-09-05 08:43:11 +0200

    Primzahlen gelöst

---
+++ exercises/04_Primzahlen.py
@@ -0,0 +1,11 @@
+primzahlen = [1,2,3,5,7]
+x = 2
+
+for i in range(2, 100):
+    if i % 2 == 0 or i %3 == 0 or i % 5 == 0 or i % 7 == 0:
+        x = x +1
+    else:
+        primzahlen.append(i)
+
+
+print(primzahlen)
