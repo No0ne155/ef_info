e4f737c9f6e0742783f69a64824c51ce6e29dd7a
No0ne155 <magnus@sunrise.ch>
2022-11-11 15:55:07 +0100

    NumTrip Refactoring

--- NumTrip/game-idea.py
+++ NumTrip/game-idea.py
@@ -1,39 +1,19 @@
-### Erste Versuche das Game zu Coden, aus diesem Code wurde neu das Dokument game.py geschrieben
-
 from random import*
-
-def numtrip():
-    v = 16          #0¦, 1gap, 2v, 3gap, 4|, 5gap, 6w, 7gap, 8|
-    w = 2**(randint(1,4))
-    x = 2**(randint(1,4))
-    y = 2**(randint(1,4))
-    z = 2**(randint(1,4))
-
-    line = ['+---------+---------+---------+---------+---------+']
-    middleline = ['|         |         |         |         |         |']
-    downline = ['|','    ',v,'    ','|','    ',w,'    ','|','    ',x,'    ','|','    ',y,'    ','|','    ',z,'    ','|']
+spielfeld = [[], [], [], [], []]
+def game():
+    print('     01        02        03        04        05   ')
+    print(' +---------+---------+---------+---------+---------+')
     for i in range(5):
-        print(line[0])
-        print(middleline[0])                            
-        for k in range(len(downline)):
-            if k < 150:
-                print(downline[k], end="")
-                v = 16
-                w = 2**(randint(1,3))
-                x = 2**(randint(1,3))
-                y = 2**(randint(1,3))
-                z = 2**(randint(1,3))
-                downline = ['|','    ',v,'    ','|','    ',w,'    ','|','    ',x,'    ','|','    ',y,'    ','|','    ',z,'    ','|']
-            elif k > 15:
-                print('too large')
-        print("")
-        print(middleline[0])
-    print(line[0])
+        for j in range(5):
+            randomnumber = 2**(randint(1,4))
+            if randomnumber < 10:
+                spielfeld[i].append(f"    {randomnumber}    ")
+            elif randomnumber < 100:
+                spielfeld[i].append(f"   {randomnumber}    ")
+    for k in range(5):
+        print(' |         |         |         |         |         |')
+        print(f"{k}|{spielfeld[i][0]}|{spielfeld[i][1]}|{spielfeld[i][2]}|{spielfeld[i][3]}|{spielfeld[i][4]}|")
+        print(' |         |         |         |         |         |')
+        print(' +---------+---------+---------+---------+---------+')
 
-def again():
-    playagain = input("Play again? (y/n)")
-    if playagain == 'y':
-        numtrip()
-    while playagain != 'n':
-        again()
-again()
+game()
--- NumTrip/game.py
+++ NumTrip/game.py
@@ -1,81 +1,19 @@
 from random import*
-playcounter = 0
-counter1 = 1
-counter2 = 2
-playagain = input("Play? (y/n)")
-def numtrip():
-    global counter1
-    global counter2
-    v = 2**(randint(1,4))          #randomized die werte für die Tabele
-    w = 2**(randint(1,4))
-    x = 2**(randint(1,4))
-    y = 2**(randint(1,4))
-    z = 2**(randint(1,4))
-    line = ['+---------+---------+---------+---------+---------+']          #design der Tabelle
-    middleline = ['|         |         |         |         |         |']    #design der Tabelle
+spielfeld = [[], [], [], [], []]
+def game():
+    print('     01        02        03        04        05   ')
+    print(' +---------+---------+---------+---------+---------+')
     for i in range(5):
-        downline = ['|    ',v,'    ',w,'    ',x,'    ',y,'    ',z,'    ','|']
-        print(line[0])                  #spaghetticode für erkennung von länge jeder zahl, und anpassung von Leerzeichen
-        print(middleline[0])            #wird noch verbessert
-        print(downline[0], end="")
-        print(downline[1], end="")
-        if downline[1] < 13:
-            downline[2] = '    '
-            print(downline[2], end="")
-        elif downline[1] > 14:
-            downline[2] = '   '
-            print(downline[2], end="")
-        print(downline[0], end="")
-        print(downline[3], end="")
-        if downline[3] < 13:
-            downline[4] = '    '
-            print(downline[4], end="")
-        elif downline[3] > 14:
-            downline[4] = '   '
-            print(downline[4], end="")
-        print(downline[0], end="")
-        print(downline[5], end="")
-        if downline[5] < 13:
-            downline[6] = '    '
-            print(downline[6], end="")
-        elif downline[5] > 14:
-            downline[6] = '   '
-            print(downline[6], end="")
-        print(downline[0], end="")
-        print(downline[7], end="")
-        if downline[7] < 13:
-            downline[8] = '    '
-            print(downline[8], end="")
-        elif downline[7] > 14:
-            downline[8] = '   '
-            print(downline[8], end="")
-        print(downline[0], end="")
-        print(downline[9], end="")
-        if downline[9] < 13:
-            downline[10] = '    '
-            print(downline[10], end="")
-        elif downline[9] > 14:
-            downline[10] = '   '
-            print(downline[10], end="")
-        print(downline[11])
-        print(middleline[0])
-        v = 2**(randint(1,4))               #generiert neue werte für neue zeilen
-        w = 2**(randint(1,4))
-        x = 2**(randint(1,4))
-        y = 2**(randint(1,4))
-        z = 2**(randint(1,4))
-    print(line[0])
+        for j in range(5):
+            randomnumber = 2**(randint(1,4))
+            if randomnumber < 10:
+                spielfeld[i].append(f"    {randomnumber}    ")
+            elif randomnumber < 100:
+                spielfeld[i].append(f"   {randomnumber}    ")
+    for k in range(5):
+        print(' |         |         |         |         |         |')
+        print(f"{k}|{spielfeld[i][0]}|{spielfeld[i][1]}|{spielfeld[i][2]}|{spielfeld[i][3]}|{spielfeld[i][4]}|")
+        print(' |         |         |         |         |         |')
+        print(' +---------+---------+---------+---------+---------+')
 
-def again():                                #fragt den Spieler ob er spielen (generieren) will
-    global playagain                        #nach 1x spielen fragt er ob man erneut spielen will
-    global playcounter
-    if playagain == 'y':
-        numtrip()
-        playcounter = playcounter + 1
-        if playcounter > 0:
-            playagain = input("Play again (y/n)")
-    while playagain != 'n':
-        again()
-        if playcounter > 0:
-            playagain = input("Play again (y/n)")
-again()
+game()
