a6be7405bb84d02db1ae788245b2c4c2d4b220e9
No0ne155 <magnus@sunrise.ch>
2022-11-18 16:34:28 +0100

    improve checking system numtrip

--- numTrip/NumTrip/game.py
+++ numTrip/NumTrip/game.py
@@ -1,11 +1,11 @@
 from random import*
-from colorama import*
 spielfeld = [[], [], [], [], []]
 checklist = []
+removed = []
 def printfield():
     global spielfeld
-    print('      0         1         2         3         4   ')
-    print(' +---------+---------+---------+---------+---------+')
+    print('\Y    0         1         2         3         4   ')
+    print('X+---------+---------+---------+---------+---------+')
     for i in range(5):
         for j in range(5):
             randomnumber = 2**(randint(1,4))
@@ -23,34 +23,50 @@ def game():
     global playerinputX
     global playerinputY
     global spielfeld
+    if len(checklist) > 0:
+        check(checklist[0], checklist[1])
+        checklist.pop(1)
+        checklist.pop(0)
     printfield()
     playerinputX = int(input("X-Axis you want to choose: "))
     playerinputY = int(input("Y-Axis you want to choose: "))
     check(playerinputX, playerinputY)
 
 def check(X, Y):
-    if spielfeld[X][Y] == spielfeld[X+1][Y]:
-        spielfeld[X+1][Y] = '    X    '
-        checklist.append(X+1)
-        checklist.append(Y)
-        print(checklist)
-    if spielfeld[X][Y] == spielfeld[X][Y+1]:
-        spielfeld[X][Y+1] = '    X    '
-        checklist.append(X)
-        checklist.append(Y+1)
-        print(checklist)
-    if spielfeld[X][Y] == spielfeld[X][Y-1]:
-        spielfeld[X][Y-1] = '    X    '
-        checklist.append(X)
-        checklist.append(Y-1)
-        print(checklist)
-    if spielfeld[X][Y] == spielfeld[X-1][Y]:
-        spielfeld[X-1][Y] = '    X    '
-        checklist.append(X-1)
-        checklist.append(Y)
-        print(checklist)
-    spielfeld[X][Y] = '    Y    '
-    game()
-def setx():
-    pass
+    if spielfeld[X][Y] != '         ':
+        if spielfeld[X][Y] == spielfeld[X+1][Y]:
+           # spielfeld[X+1][Y] = '         '
+            checklist.append(X+1)
+            checklist.append(Y)
+            removed.append(X+1)
+            removed.append(Y)
+            print(checklist)
+        if spielfeld[X][Y] == spielfeld[X][Y+1]:
+          #  spielfeld[X][Y+1] = '         '
+            checklist.append(X)
+            checklist.append(Y+1)
+            removed.append(X)
+            removed.append(Y+1)
+            print(checklist)
+        if spielfeld[X][Y] == spielfeld[X][Y-1]:
+           # spielfeld[X][Y-1] = '         '
+            checklist.append(X)
+            checklist.append(Y-1)
+            removed.append(X)
+            removed.append(Y-1)
+            print(checklist)
+        if spielfeld[X][Y] == spielfeld[X-1][Y]:
+            #spielfeld[X-1][Y] = '         '
+            checklist.append(X-1)
+            checklist.append(Y)
+            removed.append(X-1)
+            removed.append(Y)
+            print(checklist)
+        #spielfeld[X][Y] = '         '
+        removed.append(X)
+        removed.append(Y)
+        print(f'removed:{removed}')
+        game()
+
+
 game()
